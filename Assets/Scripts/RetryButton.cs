381202337c28f560b6de14c8e4b8742160469cf4
han-yerim <eternity7236@naver.com>
2025-04-08 18:26:27 +0900

    Add : 스크립트 추가
    - 타이머, 게임종료 로직

--- Assets/Scripts/RetryButton.cs
+++ Assets/Scripts/RetryButton.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class RetryButton : MonoBehaviour
 {
-
-
-
+    public void Retry()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
 }
 

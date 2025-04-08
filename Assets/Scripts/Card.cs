5a7148d23623b5100bf3a7ee082b49e320142614
wallyyoo <hyase22@naver.com>
2025-04-08 14:41:56 +0900

    Add : 카드대조 로직 추가
    -카드대조 로직 추가

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Card : MonoBehaviour
 {
+    public int idx = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,50 @@ public class Card : MonoBehaviour
     void Update()
     {
         
+    }
+
+
+    public void OpenCard()
+    {
+        anim.SetBool("isOpen", true);
+        front.SetActive(true);
+        Back.SetActive(false);
+
+            // firstCard가 비었다면
+        if (GameManager.instance.firstCard == null)
+        {   
+            // firstCard에 정보를 넣어주고
+            GameManager.instance.firstCard = this;
+        }
+        else//firstCard가 비어있지 않다면
+
+        {   //secondCard에 정보를 넘겨주고
+            GameManager.instance.secondCard = this;
+
+            //matched 함수를 불러온다
+            GameManager.instance.Matched();
+        }
+
+     }
+
+    public void DestoryCard()
+    {
+        Invoke("DestroyCardInvoke", 1.0f);
+    }
+
+    void DestroyCardInvoke()
+    {
+        Destroy(gameObject);
+    }
+    public void closeCard()
+    {
+        Invoke("closeCardInvoke", 1.0f);
+    }
+
+    void closeCardInvoke()
+    {
+        anim.SetBool("isOpen", false);
+        front.SetActive(false);
+        back.SetActive(true);
     }
 }
